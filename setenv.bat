148d82ccea64ca1cdcee36d4ad6b60cca4a93ee0
ironblaster <espositosalvy@gmail.com>
2020-09-08 11:50:49 +0200

    first commit

---
+++ setenv.bat
@@ -0,0 +1,11 @@
+echo Use JDK 1.6
+set JAVA_HOME=C:\william_dev\jdk\jdk1.6.0_06
+set PATH=%JAVA_HOME%\bin;%PATH%
+
+echo Use Tomcat 5.5.26
+set TOMCAT_HOME=C:\william_dev\server\apache-tomcat-5.5.26
+set PATH=%TOMCAT%\bin;%PATH%
+
+echo Use Ant 1.7.0
+set ANT_HOME=C:\william_dev\tools\apache-ant-1.7.0
+set PATH=%ANT_HOME%\bin;%PATH%
